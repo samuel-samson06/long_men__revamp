5158257b21ea28a753a3ea02c1d062fcbd630b56
samuel-samson06 <a.samuelsamson@gmail.com>
2024-08-15 22:55:49 +0100

    ..

--- src/components/DisplayEvent.jsx
+++ src/components/DisplayEvent.jsx
@@ -11,13 +11,13 @@ function DisplayEvent({event}) {
             <img src={`${event?.imageUrl}`} alt={event.event_name} className=' md:w-72 w-60 h-full  rounded-md'  loading={"lazy"}/>
             <section className=' font-serif max-md:w-full w-2/5 py-6 rounded-md px-5 '>
               {/* <p className=' font-semibold text-lg text-center md:text-xl underline'>Next Event</p> */}
-              <main className=' items-center justify-center flex flex-col gap-2'>
-                <p className=' text-2xl font-semibold '><span> {event?.event_name}</span></p>
-                <p className=' text-xl font-semibold '><span> {event?.event_description}</span></p>
-                <p className=' text-xl font-semibold '><span>{event?.event_location}</span></p>
-                <section className=' w-full max-md:text-sm flex justify-between'>
+              <main className=' items-start justify-center flex flex-col gap-5'>
+                <p className=' text-base md:text-xl font-semibold '><span> {event?.event_name}</span></p>
+                <p className=' text-sm md:text-lg font-semibold '><span> {event?.event_description}</span></p>
+                <p className=' text-sm md:text-lg font-semibold '><span>{event?.event_location}</span></p>
+                <section className=' w-full text-xs flex max-md:flex-col gap-2 justify-between'>
                   <p className='font-semibold '><span className=' text-sm max-sm:hidden'>Date:  </span><span>{dateEdited(event?.event_date)}</span></p>
-                  <p className='font-semibold '><span className=' text-sm'>Time:  </span><span>{event?.event_time}</span>{}</p>
+                  <p className='font-semibold '><span className=' text-xs md:text-sm'>Time:  </span><span>{event?.event_time}</span>{}</p>
                 </section>
               </main>
             </section>
--- src/pages/home/UpcomingEvent.jsx
+++ src/pages/home/UpcomingEvent.jsx
@@ -42,7 +42,7 @@ function UpcomingEvent() {
           </div>
         }
         <br />
-        <p className=' py-3 underline font-semibold capitalize text-center text-xl'>previous Events</p>
+        <p className=' py-3 underline font-semibold text-white capitalize text-center text-xl'>previous Events</p>
         <div className=' min-[550px]:flex-row min-[550px]:h-fit min-[550px]:relative justify-around md:justify-evenly  px-5 flex flex-col items-center  gap-5'>
           <img src={previous_event} alt="" className=' md:w-72 w-60 h-full  rounded-md'  loading={"lazy"}/>
           <video src={video} controls loading="lazy" className=" w-52 rounded"/>
